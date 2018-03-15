800621daba83d8e42bff7d364b2cbacc5f6ec29a
george39 <george35@utp.edu.co>
2018-03-14 22:06:38 -0500

    editar datos del perfil de usuario

--- extend/menu_asesor.php
+++ extend/menu_asesor.php
@@ -8,8 +8,9 @@
 			<div class="background">
 				<img src="https://i.ytimg.com/vi/FuHcna7q15U/maxresdefault.jpg">
 			</div> 
-			<a href="" ><img src="../usuarios/<?php echo $_SESSION['foto'] ?>" class="circle" alt=""></a>
-			<a href="" class="white-text"><?php echo $_SESSION['nombre'] ?></a> <br> <!--aqui nos traemos los datos de la bd del usuario -->
+			<a href="../perfil/index.php" ><img src="../usuarios/<?php echo $_SESSION['foto'] ?>" class="circle" alt=""></a>
+			<!--aqui cambiamos los datos del perfil de usuario -->
+			<a href="../perfil/perfil.php" class="white-text"><?php echo $_SESSION['nombre'] ?></a> <br> <!--aqui nos traemos los datos de la bd del usuario -->
 			<a href="" class="white-text"><?php echo $_SESSION['correo'] ?></a>
 		</div>
 	</li>
---
+++ perfil/perfil.php
@@ -0,0 +1,71 @@
+<!--En este archivo se actualizan los datos de perfil -->
+<?php include '../extend/header.php'; ?>
+
+<div class="row">
+	<div class="col s12">
+		<!--codigo de materialize-> components-> cards  -->
+		<div class="card">
+    <div class="card-content">
+     <h1>Editar perfil</h1>
+    </div>
+    <div class="card-tabs">
+      <ul class="tabs tabs-fixed-width">
+        <li class="tab"><a href="#datos" class="active">Datos</a></li>
+        <li class="tab"><a href="#pass">Contraseña</a></li>
+      </ul>
+    </div>
+    <div class="card-content grey lighten-4">
+
+      <div id="datos">
+      	<!-- ins_usuarios es una instancia que se agrega para identificarlo mas rapido y enctype="multipart/form-data es para enviar una foto del usuario-->
+				<form class="form" action="up_perfil.php" method="post" enctype="multipart/form-data" autocomplete="off">
+				
+
+					<!-- para el nombre de usuario -->
+					<div class="input-field">
+						<input type="text" name="nombre" title="Nombre de usuario" id="nombre" onblur="may(this.value, this.id)" required pattern="[A-Z/s ]+" value="<?php echo $_SESSION['nombre'] ?>"> <!-- el value es para mostrar los datos del usuario en los campos -->
+						<label for="nombre">Nombre completo del usuario</label>
+					</div>
+
+					<!-- para el correo de usuario -->
+					<div class="input-field">
+						<input type="email" name="correo" title="Correo electronico" value="<?php echo $_SESSION['correo'] ?> " >
+						<label for="corre">Correo electronico</label>
+					</div>
+
+					
+<button type="submit" class="btn black">Editar<i class="material-icons">send</i> </button>
+				</form> 
+      </div>
+
+
+      <div id="pass">
+      	<!-- ins_usuarios es una instancia que se agrega para identificarlo mas rapido y enctype="multipart/form-data es para enviar una foto del usuario-->
+		<form class="form" action="up_pass.php" method="post" enctype="multipart/form-data" autocomplete="off">
+			
+
+			<!-- para validar si el usuario existe en la bd -->
+			<div class="validacion"></div>
+
+			<!-- para ingresar contraseña -->
+			<div class="input-field">
+				<input type="password" name="pas1" title="CONTRASEÑA CON NUMEROS, LETRAS MAYUSCULAS Y MINUSCULAS ENTRE 8 Y 15 CARACTERES" pattern="[A-Za-z0-9]{8,15}" id="pas1" required>
+				<label for="pas1">Contraseña</label>
+			</div>
+
+			<!-- para verificar contraseña -->
+			<div class="input-field">
+				<input type="password" name="pas1" title="CONTRASEÑA CON NUMEROS, LETRAS MAYUSCULAS Y MINUSCULAS ENTRE 8 Y 15 CARACTERES" pattern="[A-Za-z0-9]{8,15}" id="pas2" required>
+				<label for="pas2">Verificar contraseña</label>
+			</div>
+      </div>
+     
+    </div>
+  </div>
+	</div>
+</div>
+
+<?php include '../extend/script.php'; ?>
+<script src="../js/validacion.js"></script> <!--aqui es para que desaparesca el boton de guardar cuando esta en el campo de contraseña -->
+</body>
+</html>
